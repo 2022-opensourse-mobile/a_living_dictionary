7e655d8b991c40277835a822229de88f1af20f27
hangbo99 <ehddnjs6409@naver.com>
2022-10-20 21:17:51 +0900

    22.10.20 v1

--- lib/main.dart
+++ lib/main.dart
@@ -42,25 +42,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      body: Center(child: Text('Hi')),
     );
   }
 }
