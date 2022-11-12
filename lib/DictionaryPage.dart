7ce74de19727e5ebb2157e0c409afab198243273
2jin8 <0208cr@naver.com>
2022-11-12 12:57:09 +0900

    11.12 DictionaryPage update

--- lib/DictionaryPage.dart
+++ lib/DictionaryPage.dart
@@ -4,8 +4,6 @@ import 'ThemeColor.dart';
 
 /* 백과사전: 청소, 빨래, 요리, 기타 */
 
-// TODO: 글자 크기 고정하기 Text() 속성 안에  textScaleFactor: 1 넣기
-
 ThemeColor themeColor = ThemeColor();
 
 // 기존 txtValue의 2번째 내용 수정
@@ -85,14 +83,15 @@ class _DictionaryPageState extends State<DictionaryPage> with TickerProviderStat
         children: [
           startxtIcon(context, '인기 TOP 10'),
           postList(context, 10),
-          slideList(context, "오늘은 대청소하는 날!", 4),
-          slideList(context, "빨래의 모든 것", 4),
-          slideList(context, "뭐 먹을지 고민된다면?", 4),
+          secondslideList(context, "오늘은 대청소하는 날!", 4, false),
+          secondslideList(context, "빨래의 모든 것", 4, false),
+          secondslideList(context, "뭐 먹을지 고민된다면?", 4, false),
         ],
       ),
     );
   }
 
+  // 텍스트 + 아이콘
   Widget startxtIcon(BuildContext context, String str) {
     return Padding(
       padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
@@ -105,135 +104,121 @@ class _DictionaryPageState extends State<DictionaryPage> with TickerProviderStat
     );
   }
 
-// 나머지 탭 화면
-  Widget otherPage(BuildContext context, String tab) {
+  // 나머지 탭 화면
+  Widget otherPage(BuildContext context, String tabName) {
     return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          secondslideList(context, '관리자가 엄선한 $tab TIP', 6),
-          Padding(
-            padding: EdgeInsets.fromLTRB(10, 10, 0, 5),
-            child: textBox(context, '최신글'),
-          ),
-          postList(context, 10),
-        ],
-      )
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            secondslideList(context, '관리자가 엄선한 $tabName TIP', 6, true),
+            Padding(
+              padding: EdgeInsets.fromLTRB(10, 10, 0, 5),
+              child: textBox(context, '최신글'),
+            ),
+            postList(context, 10),
+          ],
+        )
     );
   }
 
-  // 글 리스트
-  Widget postList(BuildContext context, int i){
+  // 게시글 리스트
+  Widget postList(BuildContext context, int postNum){
     return Container(
       child: GridView.builder(
         physics: ScrollPhysics(),
         shrinkWrap: true,
         padding: EdgeInsets.fromLTRB(5,0,5,5),
-        itemCount: i, //몇 개 출력할 건지
+        itemCount: postNum, //몇 개 출력할 건지
         itemBuilder: (context, index){
           return InkWell(
             onTap: () {
               Navigator.push(context, MaterialPageRoute(builder: (context) => tempPage(context)));
             },
-            child: card(context, index, imgValue, txtValue),
+            child: post(context, index, imgValue, txtValue),
           );
         },
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
-          childAspectRatio: 1 / 1, // 가로 세로 비율
+          // childAspectRatio: 1 / 1, // 가로 세로 비율
         ),
       ),
     );
   }
 
-  Widget card(BuildContext context, int index, List<String> imgList, List<String> textList) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
-      ),
-      elevation: 0,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(10.0),
-            // child: Image.asset(imgValue[index % imgValue.length]),
-            child: Image.asset(imgList[index % imgList.length]),
-          ),
-          Padding(
-            padding: EdgeInsets.all(5), // 게시글 제목 여백
-            child: Text(textList[index % textList.length], textScaleFactor: 1),
-            // child: Text(txtValue[index % txtValue.length]),
-         ),
-        ],
+  // 개별 게시글
+  Widget post(BuildContext context, int index, List<String> imgList, List<String> textList) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 0),
+      width: MediaQuery.of(context).size.width / 2,
+      // height: MediaQuery.of(context).size.width / 2,
+      height: MediaQuery.of(context).size.height / 4.5,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        elevation: 0,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10.0),
+              // child: Image.asset(imgValue[index % imgValue.length]),
+              child: Image.asset(imgList[index % imgList.length]),
+            ),
+            Padding(
+              padding: EdgeInsets.all(5), // 게시글 제목 여백
+              child: Text(textList[index % textList.length], textScaleFactor: 1),
+              // child: Text(txtValue[index % txtValue.length]),
+            ),
+          ],
+        ),
       ),
     );
   }
 
-  // 클릭 시, 다른 페이지 이동
+  // 클릭 시, 세부 페이지로 이동
   Widget tempPage(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("제목", textScaleFactor: 1),
         titleSpacing: 0,
         elevation: 0,
-        // centerTitle: false,
       ),
       body: ListView.builder(
         padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
         itemCount: 15,
         itemBuilder: (context, index) {
           return ListTile(
-            title: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("ListTile $index", textScaleFactor: 1),
-                Image.asset(secondimgValue[index % secondimgValue.length])
-              ],
-            )
+              title: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("ListTile $index", textScaleFactor: 1),
+                  Image.asset(secondimgValue[index % secondimgValue.length])
+                ],
+              )
           );
         },
       ),
     );
   }
 
-  // 텍스트 출력 + 가로 스크롤 리스트 출력 (1)
-  Widget slideList(BuildContext context, String str, int i){
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Divider(thickness: 0.5,),
-        Padding(
-          padding: EdgeInsets.fromLTRB(10, 10, 0, 5),
-          child: textBox(context, str),
-        ),
-        slide(context, i),
-      ],
-    );
-  }
-
-  // 텍스트 출력 + 가로 스크롤 리스트 출력 (2)
-  Widget secondslideList(BuildContext context, String str, int i){
+  // 텍스트 출력 + 가로 스크롤 리스트 출력
+  Widget secondslideList(BuildContext context, String title, int slideNum, bool iconTF){
     return InkWell(
-      onTap: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) => tempPage(context)));
-      },
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
-            child: Row(
-              children: [
-                textBox(context, str),
-                Icon(Icons.star_rounded, color: Colors.orange,),
-              ],
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => tempPage(context)));
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+              child: iconTF? startxtIcon(context, title) : textBox(context, title),
             ),
-          ),
-          slide(context, i),
-          Divider(thickness: 0.5,),
-        ],
-      )
+            slide(context, slideNum),
+            Divider(thickness: 0.5,),
+          ],
+        )
     );
   }
 
@@ -248,39 +233,8 @@ class _DictionaryPageState extends State<DictionaryPage> with TickerProviderStat
       scrollDirection: Axis.horizontal,
       padding: EdgeInsets.fromLTRB(5,0,5,5), //5055
       child: Row(
-        children: List.generate(i, (index) => slideView(index)),
+        children: List.generate(i, (index) => post(context, index, secondimgValue, txtValue)),
       ),
-    );
-  }
-
-  // 가로 스크롤 개별 게시글
-  Widget slideView(int index){
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 0),
-      // width: 220, //220
-      height: MediaQuery.of(context).size.width / 2, //198
-      width: MediaQuery.of(context).size.width / 2,
-      child: card(context, index, secondimgValue, txtValue),
-      // child: Card(
-      //   shape: RoundedRectangleBorder(
-      //     borderRadius: BorderRadius.circular(10),
-      //   ),
-      //   elevation: 0,
-      //   color: Colors.white,
-      //   child: Column(
-      //     crossAxisAlignment: CrossAxisAlignment.start,
-      //     children: [
-      //       ClipRRect(
-      //         borderRadius: BorderRadius.circular(10.0),
-      //         child: Image.asset(secondimgValue[index]),
-      //       ),
-      //       Padding(
-      //         padding: EdgeInsets.all(5),
-      //         child: Text(txtValue[index]),
-      //       ),
-      //     ],
-      //   ),
-      // ),
     );
   }
 }
