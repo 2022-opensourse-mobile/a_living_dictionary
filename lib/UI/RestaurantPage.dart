4dd4649a0245456c03be376c4ddd9d3889dda24d
2jin8 <0208cr@naver.com>
2022-11-19 02:30:04 +0900

    11.19 Update

--- lib/UI/RestaurantPage.dart
+++ lib/UI/RestaurantPage.dart
@@ -18,11 +18,30 @@ class RestaurantPage extends StatelessWidget {
 
 class Map extends StatefulWidget {
   @override
-  State<Map> createState() => _MapState();
+  State<Map> createState() => MapState();
 }
 
-class _MapState extends State<Map> {
-  Completer<GoogleMapController> _controller = Completer();
+class MapState extends State<Map> {
+  late GoogleMapController _controller;
+
+  int id = 1; // 임시 마커 id
+  final List<Marker> markers = []; // 마커를 등록할 목록
+
+  // 지도 클릭 시, 마커 등록
+  addMarker(coordinate) {
+    setState(() {
+      markers.add(Marker(
+          position: coordinate,
+          markerId: MarkerId((id++).toString()),
+          infoWindow: InfoWindow(
+              title: "마커${id-1}",
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => ShowInformation())); // 마커 클릭 시 임시 페이지로 이동
+              }
+          )
+      ));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,10 +50,19 @@ class _MapState extends State<Map> {
         mapType: MapType.normal,
         initialCameraPosition: _initialLocation,
         onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
+          setState(() {
+            _controller = controller;
+          });
         },
+        markers: markers.toSet(),
         myLocationEnabled: true, // 현재 위치를 파란색 점으로 표시
+
+        onTap: (coordinate) { // 클릭한 위치 중앙에 표시
+          _controller.animateCamera(CameraUpdate.newLatLng(coordinate));
+          addMarker(coordinate);
+        },
       ),
+
       floatingActionButton: FloatingActionButton.extended(
         onPressed: _currentLocation,
         label: Text('현재 위치'),
@@ -51,7 +79,6 @@ class _MapState extends State<Map> {
 
   // 현재 위치 구하기
   void _currentLocation() async {
-    final GoogleMapController controller = await _controller.future;
     Location location = new Location();
 
     bool _serviceEnabled;
@@ -76,15 +103,26 @@ class _MapState extends State<Map> {
     }
 
     _currentLocation = await location.getLocation();
-
-    controller.animateCamera(CameraUpdate.newCameraPosition(
-        CameraPosition(
-          bearing: 0,
-          target: LatLng(_currentLocation.latitude!, _currentLocation.longitude!),
-          tilt: 0,
-          zoom: 16.0,
-        )
+    _controller.animateCamera(CameraUpdate.newCameraPosition(
+      CameraPosition(
+        bearing: 0,
+        target: LatLng(_currentLocation.latitude!, _currentLocation.longitude!),
+        tilt: 0,
+        zoom: 17.0,
+      ),
     ));
 
+  }
+}
+
+// 마커 클릭 시 나타나는 임시 페이지
+class ShowInformation extends StatelessWidget {
+  const ShowInformation({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("임시 페이지")),
+    );
   }
 }
