77f5a26996df16541af0ac5b4ad08cf13dfcc675
hangbo99 <102962030+hangbo99@users.noreply.github.com>
2022-12-06 15:45:01 +0900

    설정 내가 쓴 게시글(수정중)

--- lib/UI/MyPage.dart
+++ lib/UI/MyPage.dart
@@ -437,6 +437,9 @@ class _MyPageState extends State<MyPage> with TickerProviderStateMixin{
 
   Widget myPosting() {
     late Logineduser user = Provider.of<Logineduser>(context, listen: false);
+
+    print("user : ${user}");
+    print("user uid : ${user.uid}");
     return Scaffold(
       appBar: AppBar(title: Text('작성한 게시물'), elevation: 0.0),
         body: Column(
@@ -444,19 +447,22 @@ class _MyPageState extends State<MyPage> with TickerProviderStateMixin{
             Expanded(child: StreamBuilder<QuerySnapshot>(
                 stream: FirebaseFirestore.instance.collection('CommunityDB').orderBy('time', descending: true).where('writer_id', isEqualTo: user.uid).snapshots(),
                 builder: (context, snapshot) {
-                  if(snapshot.connectionState == ConnectionState.waiting){
-                    return const CircularProgressIndicator();
+                  if (snapshot.hasData) {
+                    final documents = snapshot.data!.docs;
+                    if(documents.isEmpty){
+                      return const Center(child: Text("이거 왜 이럼?"),);
+                    }
+                    else{
+                      return ListView(children: documents.map((doc) {
+                            CommunityItem item = CommunityItem.getDataFromDoc(doc);
+                            return item.build(context);
+                          }).toList()
+                      );
+                    }
+                  }else{
+                    return const Center(child : Text("dasdsa"));
                   }
-                  if (!snapshot.hasData) {
-                    return const Center(child: Text("작성한 게시물이 없습니다"));
-                  }
-                  final documents = snapshot.data!.docs;
-                  return Expanded(
-                      child: ListView(children: documents.map((doc) {
-                        CommunityItem item = CommunityItem.getDataFromDoc(doc);
-                        return item.build(context);
-                      }).toList()));
-                    })
+                })
             )
           ],
         ));
@@ -468,7 +474,7 @@ class _MyPageState extends State<MyPage> with TickerProviderStateMixin{
     return Scaffold(
         appBar: AppBar(title: const Text('댓글 단 게시물'), elevation: 0.0),
         body: StreamBuilder<QuerySnapshot>(
-            stream: FirebaseFirestore.instance.collection('userInfo').doc(userDocID).collection("CommentList").snapshots(),
+            stream: FirebaseFirestore.instance.collection('userInfo').doc(userDocID).collection("CommentList").orderBy('time', descending: true).snapshots(),
             builder: (context, snap) {
               if (!snap.hasData) {
                 return const Center(child: Text("댓글을 작성한 게시글이 없습니다."));
